c8c9cce4ffe078a0230ea181820f62ab3aa6af01
TuiMao233 <wwu710632@gmail.com>
2021-03-21 15:39:20 +0800

    🐛 修复App.vue生命周期判断异常。

--- libs/lifecycle-hooks.ts
+++ libs/lifecycle-hooks.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Mr.Mao
  * @Date: 2021-03-20 15:35:47
- * @LastEditTime: 2021-03-21 14:31:40
+ * @LastEditTime: 2021-03-21 15:38:34
  * @Description: 生命周期钩子
  * @LastEditors: Mr.Mao
  * @autograph: 任何一个傻子都能写出让电脑能懂的代码，而只有好的程序员可以写出让人能看懂的代码
@@ -19,7 +19,7 @@ const createHook = <T = () => void>(lifecycle: string) => {
     }
     if (currentContext.uid === 0) {
       // 当前实例为App.vue, 不存在 页面 周期函数
-      if (!appVueLifecycleRex.test(lifecycle) || !/onShow|onHide/.test(lifecycle)) {
+      if (!appVueLifecycleRex.test(lifecycle) && !/onShow|onHide/.test(lifecycle)) {
         throw Error(`当前实例不存在 ${lifecycle} 周期函数`)
       }
     }else if (currentContext.proxy.__route__) {
